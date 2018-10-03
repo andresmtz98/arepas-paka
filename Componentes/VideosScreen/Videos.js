fb9220048ef15fc90ab9e060786d7c6e862d6fd9
dev_andres <andresfmtz08@gmail.com>
2018-10-03 13:21:40 -0500

    Factorizados algunos estilos.

--- Componentes/VideosScreen/Videos.js
+++ Componentes/VideosScreen/Videos.js
@@ -1,39 +1,99 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, StyleSheet } from 'react-native';
-import { Container, Header, Content, Left, Body, Title } from 'native-base';
-import IOSIcon from "react-native-vector-icons/Ionicons";
+import { StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { Container, Header, Content, Left, Body, Title, Right, Button, Icon } from 'native-base';
+import Video from 'react-native-video';
+
+import { CommonStyles, StyleConstants } from '../Styles';
 
 class VideosScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      videoPlayer: {
+        repeat: false, 
+        rate: 1, 
+        volume: 1, 
+        muted: false, 
+        resizeMode: 'contain', // video's mode: none, cover, stretch, contain 
+        duration: 0.0, // video's duration set on event onLoad 
+        currentTime: 0.0, // set on event onProgress 
+        paused: true, // check if video is pausing or not 
+        rateText: '1.0', // rate value in component Picker 
+        pausedText: 'Play', // view to user: 'Play' - when video is pausing, 'Pause' - when video is playing 
+        hideControls: false, // hide control button when video is playing and show it when user clicks on video
+      },
     };
+  }
+
+  onLoad = data => {
+    this.setState({ videoPlayer: { duration: data.duration }});
+  }
+
+  onPress = data => {
+    this.setState({ videoPlayer: { currentTime: data.currentTime }})
+  }
+
+  onEnd = data => {
+    this.setState({ videoPlayer: { pausedText: 'Reproducir', paused: true }});
+    this.video.seek(0);
   }
 
   render() {
     return (
-      <Container>
-        <Header>
+      <Container style={CommonStyles.baseColor}>
+        <Header transparent noShadow>
           <Left>
-            <TouchableOpacity
-              style={styles.touchableOpacity} 
+            <Button
+              transparent 
               onPress={() => this.props.navigation.toggleDrawer()} >
-              <IOSIcon name="ios-menu" size={30} />
-            </TouchableOpacity>
+              <Icon 
+                name="ios-menu" 
+                type="Ionicons"                 
+                style={CommonStyles.baseColor_Font}
+              />
+            </Button>
           </Left>
           <Body>
-            <Title>Videos</Title>
+            <Title style={CommonStyles.baseColor_Font}>Videos</Title>
           </Body>
+          <Right />
         </Header>
+        <Content style={styles.content}>
+          <TouchableWithoutFeedback 
+          onPress={() => this.setState({ paused: !this.state.paused })}>
+            <Video
+              style={styles.videoPlayer} 
+              ref={( ref: Video) => { this.video = ref }}
+              source={require('./../../Servicios/Videos/video.mp4')}
+              repeat={this.state.videoPlayer.repeat}
+              rate={this.state.videoPlayer.rate}
+              volume={this.state.videoPlayer.volume}
+              muted={this.state.videoPlayer.muted}
+              resizeMode={this.state.videoPlayer.resizeMode}
+              paused={this.state.videoPlayer.paused}
+              onLoad={this.onLoad}
+              onProgress={this.onProgress}
+              onEnd={this.onEnd}
+            />
+          </TouchableWithoutFeedback>
+        </Content>
       </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  touchableOpacity: {
-    marginLeft: 10,
-  }
+  content: {
+    borderTopColor: StyleConstants.BASE_COLOR_FONT_HEX,
+    borderTopWidth: 1,
+  },
+  videoPlayer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
 });
 
 export default VideosScreen;
